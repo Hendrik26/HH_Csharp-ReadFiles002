def346625d8eea3d48954168dc616bc77b0cbadf
Ullrich Herdler <ullrich.herdler@gmail.com>
2020-01-21 15:24:50 +0100

    HH_Csharp-ReadFiles002 First Commit!!!

--- HH_Csharp-ReadFiles002/Form1.cs
+++ HH_Csharp-ReadFiles002/Form1.cs
@@ -16,5 +16,10 @@ namespace HH_Csharp_ReadFiles002
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
