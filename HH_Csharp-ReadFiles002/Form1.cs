c5b5131a1a26f6c9196e069228748ec2454ecd07
Ullrich Herdler <ullrich.herdler@gmail.com>
2020-01-23 14:26:50 +0100

    Read from File works.

--- HH_Csharp-ReadFiles002/Form1.cs
+++ HH_Csharp-ReadFiles002/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace HH_Csharp_ReadFiles002
 {
@@ -23,6 +24,17 @@ namespace HH_Csharp_ReadFiles002
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = string.Empty;
+            StreamReader streamReader = File.OpenText(fileName);
+            string line = string.Empty;
+            while ((line = streamReader.ReadLine()) != null)
+            {
+                textBox1.Text += line + Environment.NewLine;
+            }
         }
     }
 }
